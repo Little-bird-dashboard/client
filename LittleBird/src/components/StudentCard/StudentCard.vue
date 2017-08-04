be79553ca7b5db962fa5d990334c23d8a619a320
Will Carpenter <willcarpenteriii@gmail.com>
2017-08-03 18:24:31 -0600

    layout of child card bootstraped

--- LittleBird/src/components/StudentCard/StudentCard.vue
+++ LittleBird/src/components/StudentCard/StudentCard.vue
@@ -1,33 +1,77 @@
 <template>
     <div id="StudentCard">
         <div class="card-block">
-
-            <h4 class="card-title">{{studentData[0].name}}</h4>
-            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="card-link">Card link</a> <a href="#" class="card-link">Another link</a>
-
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-2">
+                        <img width="120" height="120" v-bind:src="studentData[0].image_url"/>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <h1>{{ studentData[0].name }}</h1>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-6">
+                           <span>
+                               <h4>Student ID: </h4><h5>123456</h5>
+                           </span>
+                            </div>
+                            <div class="col-lg-6">
+                             <span>
+                               <h4>Student ID: </h4><h5>123456</h5>
+                           </span>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <h4>Student ID: </h4>
+                                <h5>123456</h5>
+                            </div>
+                            <div class="col-lg-6">
+                                <h4>Student ID: </h4>
+                                <h5>123456</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="row">
+                            <div class="col-lg-3 pull-right">
+                                <i class="fa fa-times fa-3x" aria-hidden="true"></i>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-3 pull-right">
+                                <button class="btn btn-success">edit notes</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-
 </template>
 
 <script>
 	export default {
 		name:    'StudentCard',
-    props: ['studentData'],
+		props:   ['studentData'],
 		data() {
-			return {
-      }
+			return {}
 		},
-    mounted() {
-      //do something after mounting vue instance
-      console.log(this.studentData)
-    },
-		methods: {
-    }
+		mounted() {
+			//do something after mounting vue instance
+			console.log(this.studentData)
+		},
+		methods: {}
 	}
 </script>
 
 <style>
+    h4, h5 {
+        display: inline;
+    }
+
+
 </style>
