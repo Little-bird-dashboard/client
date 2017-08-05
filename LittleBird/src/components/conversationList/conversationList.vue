38ae63b9ff4f14380a0ccaefb89e501718a3b245
Marlena Baker <baker.marlena@gmail.com>
2017-08-05 09:02:54 -0600

    conversation list componentes added

---
+++ LittleBird/src/components/conversationList/conversationList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="conversationList" v-for="message in messages">
+    <conversationMessage :messageData="message"></conversationMessage>
+  </div>
+</template>
+<script>
+import conversationMessage from '../conversationMessage/conversationMessage'
+
+export default {
+  name: 'conversationList',
+  props: ['messages']
+  components: {
+    conversationMessage
+  }
+  data () {
+
+  },
+}
+</script>
+<style scoped>
+</style>
---
+++ LittleBird/src/components/conversationMessage/conversationMessage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="conversationMessage">
+    <div class="row">
+      <div class="col-lg-1">
+        <img />
+      </div>
+      <div class="col-lg=11">
+        <h4>{{messageData.communication_type}} from {{messageData.first_name}} {{messageData.last_name}}</h4>
+        <div class="messageBody">
+          {{messageData.raw_body}}
+        </div>
+        <small>{{moment(messageData.timestamp).format('MMMM Do')}} at {{moment(messageData.timestamp).format('h:mm a')}}</small>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'conversationMessage',
+  props: ['messageData']
+  data () {
+
+  }
+}
+</script>
+<style scoped>
+.messageBody{
+  corner-radius:10px;
+}
+</style>
